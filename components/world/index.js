60696d3bbe8609d9a9a722cc85556d0f005c0ea1
philburrrt <philbertburrow@protonmail.com>
2022-08-06 20:38:18 -0500

    init

--- components/world/index.js
+++ components/world/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Erika } from './npc/Erika'
 
 export default function World() {
   return (
@@ -9,11 +10,20 @@ export default function World() {
       <gliding />
       <flying />
       <rigidbody>
-        <model scale={[2.5,2.5,2.5]}position={[0,-0.12,0]} src="lab_base_13.glb" />
-        <model scale={[2.5,2.5,2.5]}position={[0,-0.57,0]} src="floor.glb" />
+        <model
+          scale={[2.5, 2.5, 2.5]}
+          position={[0, -0.12, 0]}
+          src="lab_base_13.glb"
+        />
+        <model
+          scale={[2.5, 2.5, 2.5]}
+          position={[0, -0.57, 0]}
+          src="floor.glb"
+        />
       </rigidbody>
+      <Erika position={[-4.332, 1.037, 3.226]} teddyPosition={[0, 0, 3]} />
       <audio autoplay={true} loop={false} volume={0.5} src="resistor.mp3" />
-      <spawn position={[0,2,0]} />
+      <spawn position={[0, 2, 0]} />
     </environment>
   )
 }
